2ec23628db0a6fb0d5179a1783fa60c64e1754a5
glitchgg <glitchsupp@gmail.com>
2021-03-15 10:09:02 -0700

    added ContactScreen.js and modified HomeStack.js

--- src/screens/Home/HomeStack.js
+++ src/screens/Home/HomeStack.js
@@ -1,16 +1,17 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import RegisterScreen from './RegisterScreen';
+import ContactScreen from './ContactScreen';
 import React from 'react';
 
 const Stack = createStackNavigator();
 
 function HomeStack() {
-
     return (
-        <Stack.Navigator initialRouteName="Home" >
+        <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Contact" component={ContactScreen} options={{ headerShown: false}} />
         </Stack.Navigator>
     );
 }
