9bc2fcfe868a7e7e45495c4f5437cf051103ceaf
glitchgg <patparkk@gmail.com>
2021-03-20 15:18:47 -0700

    contactscreen debugging 2

--- src/screens/Home/ContactScreen.js
+++ src/screens/Home/ContactScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text, View, Button } from 'react-native';
+import { SafeAreaView, Text, View, } from 'react-native';
 import { Button } from 'react-native-paper';
 
 const ContactScreen = (props) => {
