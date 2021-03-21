c731c03ae2aeef2765c7b9edd7077ca5716789cf
jtagaca <jtagaca@csub.edu>
2021-03-20 19:41:57 -0700

    added card view to newt

--- src/screens/Home/newt.js
+++ src/screens/Home/newt.js
@@ -44,6 +44,24 @@ function newt(props) {
           data={restaurants}
           renderItem={({ restaurants }) => <Text>{restaurants.alias}</Text>}
         />
+        {/* const MyComponent = () => (
+        <Card>
+          <Card.Title
+            title="Card Title"
+            subtitle="Card Subtitle"
+            left={LeftContent}
+          />
+          <Card.Content>
+            <Title>Card title</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
+        ); */}
       </SafeAreaView>
     );
   }
