058d059c9dfeac910fe8465f92f604cb65b05432
jtagaca <jtagaca@csub.edu>
2021-03-20 19:40:07 -0700

    error at newt.js

--- src/screens/Home/newt.js
+++ src/screens/Home/newt.js
@@ -1,64 +1,65 @@
-
-import { PaperProvider, Avatar, Button, Card, Title, Paragraph,ActivityIndicator } from 'react-native-paper';
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
-import { fetchRestaurants } from '../../redux/actions/RestaurantActions';
-import { useSelector, useDispatch } from 'react-redux';
-
+import {
+  PaperProvider,
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  ActivityIndicator,
+} from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, Text, View, StyleSheet, FlatList } from "react-native";
+import { fetchRestaurants } from "../../redux/actions/RestaurantActions";
+import { useSelector, useDispatch } from "react-redux";
 
 //this is still in progress
 function newt(props) {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect( () => {
-        dispatch(fetchRestaurants("burger"))
-    }, []);
+  useEffect(() => {
+    dispatch(fetchRestaurants("burger"));
+  }, []);
 
-    const restaurants = useSelector(state => (state.restaurant.restaurants) ? state.restaurant.restaurants.businesses : null)
-
-    const isLoading = useSelector(state => state.restaurant.isFetchingRestaurants)
-
-    if (isLoading || !restaurants)
-    {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <ActivityIndicator size="large" />
-                </View>
-            </SafeAreaView>
-        )
-    }
-    else
-    {
-        return (
-            
-            <Card>
-            <Card.Title title="Card Title" subtitle="Card Subtitle"  />
-            <Card.Content>
-              <Title>Card title</Title>
-              <Paragraph>Card content</Paragraph>
-            </Card.Content>
-            <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-            <Card.Actions>
-              <Button>Cancel</Button>
-              <Button>Ok</Button>
-            </Card.Actions>
-          </Card>
-        )
-    }
+  const restaurants = useSelector((state) =>
+    state.restaurant.restaurants
+      ? state.restaurant.restaurants.businesses
+      : null
+  );
+  console.log(restaurants);
+  const isLoading = useSelector(
+    (state) => state.restaurant.isFetchingRestaurants
+  );
+  if (isLoading || !restaurants) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size="large" />
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+     <SafeAreaView>
+        <FlatList data={restaurants}
+        renderItem={({restaurants})=>(
+        <Text>{restaurants.alias}</Text>
+        )}/>
+           
+    </SafeAreaView>
+    )
+  
+}
 }
 
 export default newt;
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        backgroundColor:"blue",
-        textAlign: 'center'
-
-    },
-    text:{
-        color:"blue",
-
-    }
-})
+  background: {
+    flex: 1,
+    backgroundColor: "blue",
+    textAlign: "center",
+  },
+  text: {
+    color: "blue",
+  },
+});
