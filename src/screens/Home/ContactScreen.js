d06cb11d6932b8ddf5500dbbd5502906aa06893e
glitchgg <patparkk@gmail.com>
2021-03-28 13:16:35 -0700

    added some navigation behavior to submit and cancel buttons in contact form

--- src/screens/Home/ContactScreen.js
+++ src/screens/Home/ContactScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, Image} from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Image, Alert} from 'react-native';
 import { Button } from 'react-native-paper';
 
 const ContactScreen = (props) => {
@@ -10,10 +10,10 @@ const ContactScreen = (props) => {
                     style={{width: 100, height: 100}}
                     source={require("../../../assets/templogo.png")}
                 />
-                <Button onPress={() => props.navigation.navigate("Submit")}>
+                <Button onPress={() => Alert.alert('Message sent!', 'Our team will reach out to you within 1-2 business days', [{text: "OK", onPress: () => props.navigation.navigate("Home")}])}>
                     Submit
                 </Button>
-                <Button onPress={() => props.navigation.navigate("Home")}>
+                <Button onPress={() => Alert.alert('Discard message?','', [{text: "Yes", onPress: () => props.navigation.navigate("Home")}, {text: "No"}])}>
                     Cancel
                 </Button>
             </View>
