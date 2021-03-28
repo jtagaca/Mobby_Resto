08cfa28a86f743b48ff58cf25ede2f5563e6c3cf
glitchgg <patparkk@gmail.com>
2021-03-28 11:55:17 -0700

    more testing

--- src/screens/Home/ContactScreen.js
+++ src/screens/Home/ContactScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text, View, Image} from 'react-native';
 import { Button } from 'react-native-paper';
-//test
+// more test
 const ContactScreen = (props) => {
     return (
         <SafeAreaView style={ styles.container }>
