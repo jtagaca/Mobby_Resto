4c9c5c17aea10da2677ff72a7895d7d777e60161
glitchgg <patparkk@gmail.com>
2021-04-06 02:00:01 -0700

    textbox modifications, initial form-hook attempt

--- src/screens/Home/ContactScreen.js
+++ src/screens/Home/ContactScreen.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import emailjs from "emailjs-com";
 import { StyleSheet, SafeAreaView, Text, View, Image, Alert, TextInput} from 'react-native';
 import { Button } from 'react-native-paper';
+import { useForm, Controller } from "react-hook-form";
+//npm install emailjs-com --save
 
 const ContactScreen = (props) => {
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'template_n7xrgju', e.target, 'user_oSCxckXQXJ0r5g1cCnlau')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+        }
     return (
         <SafeAreaView style={ styles.container }>
             <View style={{ flex: 1 }}>
-                <Image 
-                    style={{width: 100, height: 100}}
-                    source={require("../../../assets/templogo.png")}
-                />
+            <Image style={{width: 100, height: 100}} source={require("../../../assets/templogo.png")}/>
+            <TextInput type="text" className="form-control" placeholder="Name" name="name" style={styles.input}/>
+            <TextInput type="text" className="form-control" placeholder="Email" name="email" style={styles.input}/>
+            <TextInput type="text" className="form-control" placeholder="Message" name="message" multiline style={styles.input}/> 
+            <Button type="submit" value="Send Message" onPress={() => Alert.alert('Message sent!', 'Our team will reach out to you within 1-2 business days', [{text: "OK", onPress: () => props.navigation.navigate("Home")}])}>
+                Submit
+            </Button>
+            <Button onPress={() => Alert.alert('Discard message?','', [{text: "Yes", onPress: () => props.navigation.navigate("Home")}, {text: "No"}])}>
+                Cancel
+            </Button>
+            </View>
+        </SafeAreaView>
+    )
+}
+/*
                 <Text> Email: </Text>
                 <TextInput style={styles.input}/>
                 <Text> Name: </Text>
@@ -17,18 +41,14 @@ const ContactScreen = (props) => {
                 <Text> Subject </Text>
                 <TextInput style={styles.input}/>
                 <Text> Message: </Text>
-                <TextInput style={styles.input}/>
+                <TextInput style={styles.input}/> 
                 <Button onPress={() => Alert.alert('Message sent!', 'Our team will reach out to you within 1-2 business days', [{text: "OK", onPress: () => props.navigation.navigate("Home")}])}>
                     Submit
                 </Button>
                 <Button onPress={() => Alert.alert('Discard message?','', [{text: "Yes", onPress: () => props.navigation.navigate("Home")}, {text: "No"}])}>
                     Cancel
                 </Button>
-            </View>
-        </SafeAreaView>
-    )
-}
-
+*/
 //const messageStyle = {height: 100};
 
 const styles = StyleSheet.create({
@@ -47,3 +67,36 @@ const styles = StyleSheet.create({
     }
 });
 export default ContactScreen;
+
+/*
+import './ContactUs.css';
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <div>
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+    </div>
+  );
+}
+*/
