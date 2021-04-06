25d39d8a308727881d44c70e01547ee4babf803c
jtagaca <jtagaca@csub.edu>
2021-04-05 19:50:04 -0700

    fixed uncaught bug on registerscreen

--- src/screens/Home/RegisterScreen.js
+++ src/screens/Home/RegisterScreen.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import { SafeAreaView, Text, View } from 'react-native';
-// import { Button } from 'react-native-paper';
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
-// const RegisterScreen = (props) => {
-//     return (
-//         <SafeAreaView style={{ flex: 1 }}>
-//             <View style={{ flex: 1 }}>
-//                 <Text style={{ fontSize: 30 }}>
-//                     This is the Register Screen.
-//                 </Text>
-//                 <Button onPress={() => props.navigation.goBack()}>
-//                     Go Back
-//                 </Button>
-//             </View>
-//         </SafeAreaView>
-//     )
-// }
+const RegisterScreen = (props) => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 30 }}>
+                    This is the Register Screen.
+                </Text>
+                <Button onPress={() => props.navigation.goBack()}>
+                    Go Back
+                </Button>
+            </View>
+        </SafeAreaView>
+    )
+}
 
-// export default RegisterScreen;
+export default RegisterScreen;
 // import React from 'react'
 // import { Provider } from 'react-native-paper'
 // import { NavigationContainer } from '@react-navigation/native'
