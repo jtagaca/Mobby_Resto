9e2a7214bfd3db0ef475b6a394dfc7067d1e7439
jtagaca <jtagaca@csub.edu>
2021-04-05 19:12:08 -0700

    phone button bug found

--- src/screens/Home/PhoneButton.js
+++ src/screens/Home/PhoneButton.js
@@ -2,10 +2,31 @@ import React, { Component } from "react";
 import {Linking,Platform,TouchableOpacity,Text} from "react-native";
 export default class MakeCall extends Component {
 
- dialCall = (number) => {
+
+
+//  Render(){
+//         return(
+//                 <TouchableOpacity
+//                    style={{
+//                    height: 30,
+//                    width: 30,
+//                    backgroundColor: "#329df4",
+//                    alignItems: "center",
+//                    justifyContent: "center",
+//                    borderRadius: 5
+//                    }}
+//                  onPress={()=>{this.dialCall(123456789)}}
+//                 >
+//                 <Text>Phone</Text>
+//                 </TouchableOpacity>
+//               )
+//   }
+
+}
+function CallNum (number) {
     let phoneNumber = '';
     if (Platform.OS === 'android') { phoneNumber = `tel:${number}`; }
     else {phoneNumber = `telprompt:${number}`; }
     Linking.openURL(phoneNumber);
  };
-};
+export {CallNum};
--- src/screens/Home/newt.js
+++ src/screens/Home/newt.js
@@ -8,15 +8,24 @@ import {
   ActivityIndicator,
 } from "react-native-paper";
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, Text, View, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Linking,
+  Platform
+} from "react-native";
 import { fetchRestaurants } from "../../redux/actions/RestaurantActions";
 import { useSelector, useDispatch } from "react-redux";
 import Bot from "./botNav";
 import Search from "./search";
-import TestScreen from "./TestScreen"
-import ImageViewer from 'react-native-image-zoom-viewer';
-import { Rating, AirbnbRating } from 'react-native-ratings';
-
+import TestScreen from "./TestScreen";
+import ImageViewer from "react-native-image-zoom-viewer";
+import { Rating, AirbnbRating } from "react-native-ratings";
+import { CallNum } from "./PhoneButton";
 
 //this is still in progress
 function newt(props) {
@@ -26,14 +35,20 @@ function newt(props) {
     dispatch(fetchRestaurants("burger"));
   }, []);
 
+  const CallNum = (number) => {
+    let phoneNumber = '';
+    if (Platform.OS === 'android') { phoneNumber = `tel:${number}`; }
+    else {phoneNumber = `telprompt:${number}`; }
+    Linking.openURL(phoneNumber);
+ };
+
   const restaurants = useSelector((state) =>
     state.restaurant.restaurants
       ? state.restaurant.restaurants.businesses
       : null
   );
 
-  
-  console.log(restaurants.display_phone); // I need to store this obkect in a var use this for removing the - 
+  console.log(restaurants.display_phone); // I need to store this obkect in a var use this for removing the -
   // const categories= {restaurant.categories.alias};
   // console.log(categories);
   const isLoading = useSelector(
@@ -47,58 +62,50 @@ function newt(props) {
         </View>
       </SafeAreaView>
     );
-    
-  
   } else {
     return (
       <SafeAreaView>
-         
         <Search style={styles.background} />
         <FlatList
           keyExtractor={(item) => item.id}
           data={restaurants}
-          
           renderItem={({ item }) => (
-            
             <View style={styles.container}>
-            <Card style={ styles.card, styles.spacing}>
-              <Text>{item.categories.alias}</Text>
+              <Card style={(styles.card, styles.spacing)}>
+                <Text>{item.categories.alias}</Text>
 
-              {/* use the method replace to remove the dashes but we need to store the output first to a variable*/}
-              <Card.Content>
-                <Title>{item.alias}</Title>
-              </Card.Content>
-       
-              <Card.Cover 
-              
-              source={{ uri: item.image_url }} />
-              <Card.Actions>
-                <TouchableOpacity style={ styles.appButtonContainer}>
-                <Button style={ styles.appButtonText}>More Info</Button>
-                </TouchableOpacity>
-                {/* <Button>Placeholder</Button> */}
-                <Text style={styles.text}>Phone: {item.phone}</Text>
-                {/* need to move  */}
-                <View style={styles.rating}>
-                <Rating 
-                
-                type='custom'
-                ratingCount= {item.rating}
-                imageSize={25}
-                ratingBackgroundColor= 'grey'
-                ratingColor= "lightblue"
-                ratingCount={5}
-                />
-                </View>
-              </Card.Actions>
-            </Card>
+                {/* use the method replace to remove the dashes but we need to store the output first to a variable*/}
+                <Card.Content>
+                  <Title>{item.alias}</Title>
+                </Card.Content>
+
+                <Card.Cover source={{ uri: item.image_url }} />
+                <Card.Actions>
+                  <TouchableOpacity style={styles.appButtonContainer}>
+                    <Button style={styles.appButtonText}>More Info</Button>
+                  </TouchableOpacity>
+                  {/* <Button>Placeholder</Button> */}
+                  <TouchableOpacity>
+                  {/* <Button onClick={CallNum (item.phone)}>Phone </Button> */}
+                  </TouchableOpacity>
+                  {/* need to move  */}
+                  <View style={styles.rating}>
+                    <Rating
+                      type="custom"
+                      ratingCount={item.rating}
+                      imageSize={25}
+                      ratingBackgroundColor="grey"
+                      ratingColor="lightblue"
+                      ratingCount={5}
+                    />
+                  </View>
+                </Card.Actions>
+              </Card>
             </View>
           )}
-          
         />
         {/* <Bot/> */}
         {/* <Button onPress={() => navigation.navigate(TestScreen)}>Hit me</Button> //not working */}
-       
       </SafeAreaView>
     );
   }
@@ -107,48 +114,50 @@ function newt(props) {
 export default newt;
 
 const styles = StyleSheet.create({
-  
-  container:{
+  container: {
     backgroundColor: "lightblue",
   },
-  text: {
-    
-  },
+  text: {},
   spacing: {
-    marginTop:40,
-    paddingLeft:0,
-    paddingRight:0,
-    paddingBottom:0
+    marginTop: 40,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
   },
   card: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 20,
     width: 10,
     borderRadius: 1,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginBottom: 3,
     marginTop: 3,
     borderLeftWidth: 5,
-    borderLeftColor: '#ea7e7a'
-    
+    borderLeftColor: "#ea7e7a",
   },
   rating: {
-    flex: 1
-   
-  }, 
+    flex: 1,
+  },
   appButtonContainer: {
     elevation: 8,
     backgroundColor: "white",
     borderRadius: 10,
     paddingVertical: 10,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   appButtonText: {
     fontSize: 18,
     color: "#fff",
     fontWeight: "bold",
     alignSelf: "center",
-    textTransform: "uppercase"
-  }
+    textTransform: "uppercase",
+  },
+  phone: {
+    height: 30,
+    width: 30,
+    backgroundColor: "#329df4",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+  },
 });
-
